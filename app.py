206907fb62e9f79b3eba9a27a5e4ccc58f82321c
exaucemwatcha41-cmyk <exaucemwatcha41@gmail.com>
2026-09-10 16:27:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ Si on dit bonjour: "Mbote Boss! 🔥 Je vais bien, prêt à t'aider!"
 
     try:
         completion = client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
+            model=model="openai/gpt-oss-20b",
             messages=[system] + messages,
             temperature=0.7,
             max_tokens=500
